6f2c77ac74aea3cb2654ac36f4c996165c0ad9f2
Moshi Musukwa <musukwamoshi@gmail.com>
2018-11-28 02:45:28 +0200

    Added index and show method to show controller

--- src/main/java/com/mosaic/pets/config/WebConfig.java
+++ src/main/java/com/mosaic/pets/config/WebConfig.java
@@ -3,13 +3,13 @@ package com.mosaic.pets.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"com.mosaic.pets.controller"})
 
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 	
 	
 
--- src/main/java/com/mosaic/pets/controller/PetController.java
+++ src/main/java/com/mosaic/pets/controller/PetController.java
@@ -1,6 +1,9 @@
 package com.mosaic.pets.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,31 +17,33 @@ public class PetController {
 	@Autowired
 	private PetService petService;
 	
+	//pull all pets
 	@RequestMapping("/")
 	public ModelAndView index() {
 		
-		
-		
-		
+		ModelAndView mv = new ModelAndView("index");
+		List<Pet> list = petService.list();
+		mv.addObject("lists", list);
+		return mv;
 		
 	}
 	
+	//show one pet
 	@RequestMapping("show/{id}")
 	public ModelAndView show () {
 		
+		ModelAndView mv = new ModelAndView("show");
+		Pet pet = petService.get(id);
+		mv.addObject("pet",pet);
 		
-		
-		
-		
-		
-		
+		return mv;
 	}
 	
 	
-	@RequestMapping("add/")
+	@RequestMapping("add/pet")
 	public void save() {
 		
-		
+		long id = petService.save(pet);
 		
 		
 		
@@ -73,8 +78,6 @@ public class PetController {
 	
 	@RequestMapping("delete/")
     public void delete() {
-		
-		
 		
 		
 		
